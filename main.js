79f251717b4edc91c1674448212f04cac2f81e5f
Adios <adiosf6f+github@gmail.com>
2014-06-15 23:30:06 +0800

    Improve with range capability.
    * able to be check whether a range is in the db.
    * able to merge the new range with existing ranges.

--- main.js
+++ main.js
@@ -1,49 +1,40 @@
-var ap = require('http').createServer(server),
-	redis = require('redis'),
-	client = redis.createClient('/tmp/redis.sock'),
+var	async = require('async'),
+	server = require('http').createServer(processRequest),
+	redis = require('redis').createClient('/tmp/redis.sock'),
+
 	level = /\/([^\/]+)(?:\/([^\/]+))?/;
 
-ap.listen(3216);
+server.listen(3216);
 
-client.on('error', function(err) {
-	console.log('Redis error: ' + err);
+redis.on('error', function(err) {
+	console.error('Redis error: ' + err);
 });
 
-function server(req, response) {
-	var	video, range, segment,
-		respond = respondOn.bind(response),
-		m = req.url.match(level);
+function processRequest(req, response) {
+	var	video, range,
+		match = req.url.match(level),
+		respond = respondOn.bind(response);
 
-	if (!m) {
-		respond(404)();
-		return;
-	}
+	if (!match)
+		return respond(404)();
 
-	video = m[1];
-	range = m[2];
-	segment = video + ':' + range;
+	if (match[2])
+		range = match[2].split('-');
+	video = match[1];
 
 	switch (req.method) {
 	case 'GET':
-		console.log('[%s] %s %s', req.connection.remoteAddress, req.method, req.url);
-		if (range)
-			getSegmentAddresses(segment, respond(200), respond(404));
-		else
-			getVideoAddresses(video, respond(200), respond(404));
+		range ? getSegmentAddresses(video, parseInt(range[0]), parseInt(range[1]), respond(200), respond(404))
+			  : getAllSegmentAddresses(video, respond(200), respond(404));
 		break;
 	case 'POST':
-		console.log('[%s] %s %s', req.connection.remoteAddress, req.method, req.url);
-		putSegmentAddress(segment, req.connection.remoteAddress, respond(200));
+		putSegmentAddress(video, parseInt(range[0]), parseInt(range[1]), req.connection.remoteAddress, respond(200));
 		break;
 	case 'DELETE':
-		if (range)
-			purgeSegmentAddresses(segment, respond(200));
-		else
-			purgeVideoAddresses(video, respond(200));
+		purgeAllSegmentAddresses(video, respond(200));
 		break;
 	default:
-		respond(200)();
-		break;
+		return respond(200)();
 	}
 }
 
@@ -55,36 +46,73 @@ function respondOn(code) {
 	};
 }
 
-function getVideoAddresses(name, found, notFound) {
-	client.keys(name + '*', function (err, reply) {
-		(reply.length == 0) ? notFound() : found(reply);
+function randomlyTakeN(n, a) {
+	var i = len = a.length, j, tmp,
+		mm = m = (n > i) ? i : n;
+
+	for (; m; j = Math.floor(Math.random() * i), tmp = a[--i], a[i] = a[j], a[j] = tmp, --m);
+	return a.slice(len - mm);
+}
+
+function getAllSegmentAddresses(key, exists, notExists) {
+	redis.zrangebyscore(key, '-inf', '+inf', 'WITHSCORES', function(err, addrsWithScore) {
+		(addrsWithScore.length == 0) ? notExists() : exists(addrsWithScore);
 	});
 }
 
-function getSegmentAddresses(key, found, notFound) {
-	client.srandmember(key, 2, function(err, reply) {
-		(reply.length == 0) ? notFound() : found(reply);
-	});
-}
+function getSegmentAddresses(key, begin, end, exists, notExists) {
+	redis.zrangebyscore(key, '-inf', begin, function(err, addrs) {
+		if (addrs.length == 0)
+			return notExists();
 
-function putSegmentAddress(key, address, callback) {
-	client.sadd(key, address, function(err, reply) {
-		callback(reply);
-	});
-}
-
-function purgeSegmentAddresses(key, callback) {
-	client.del(key, function(err, reply) {
-		callback(reply);
-	});
-}
-
-function purgeVideoAddresses(name, callback) {
-	client.keys(name + '*', function(err, reply) {
-		reply.forEach(function(key) {
-			client.del(key);
+		var covered = addrs.filter(function(addrWithEnd) {
+			return parseInt(addrWithEnd) >= end;
+		}).map(function(addrWithEnd) {
+			return addrWithEnd.split(' ')[1];
 		});
-		// don't care
-		callback();
+
+		return (covered.length == 0)
+			? notExists()
+			: exists(randomlyTakeN(2, covered));
+	});
+}
+
+function putSegmentAddress(key, begin, end, address, callback) {
+	redis.zrangebyscore(key, '-inf', end + 1, 'WITHSCORES', function(err, addrsWithScore) {
+		var minBegin = begin,
+			maxEnd = end,
+			covered = [];
+
+		for (var i = 0, len = addrsWithScore.length; i < len; i += 2) {
+			var str = addrsWithScore[i].split(' '),
+				segmentAddr = str[1],
+				segmentBegin = parseInt(addrsWithScore[i + 1]),
+				segmentEnd = parseInt(str[0]);
+
+			if (segmentAddr != address || segmentEnd < begin - 1)
+				continue;
+
+			if (begin >= segmentBegin && end <= segmentEnd)
+				return callback('Already be covered.');
+
+			covered.push(addrsWithScore[i]);
+
+			minBegin = (segmentBegin > minBegin) ? minBegin : segmentBegin;
+			maxEnd = (segmentEnd > maxEnd) ? segmentEnd : maxEnd;
+		}
+
+		async.each(covered, function(m, callback) {
+			redis.zrem(key, m, callback);
+		}, function(err) {
+			redis.zadd(key, minBegin, maxEnd + ' ' + address, function(err, reply) {
+				callback(reply);
+			});
+		});
+	});
+}
+
+function purgeAllSegmentAddresses(key, callback) {
+	redis.del(key, function(err, reply) {
+		callback(reply);
 	});
 }
